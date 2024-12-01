f9a1a3726b82b8c07a33a49d885e2036023a73d9
ji-circle <sp202401jw@gmail.com>
2024-12-02 01:01:41 +0900

    json

--- app/src/main/java/com/example/chatapp/feature/auth/signin/SignInScreen.kt
+++ app/src/main/java/com/example/chatapp/feature/auth/signin/SignInScreen.kt
@@ -40,36 +40,26 @@ fun SignInScreen(
         mutableStateOf("")
     }
 
-    //UI 구성은 보통 scaffold로 구성함
-    //scaffold에 padding(it) 또는 paddingValue -> 사용 안하면 붉은밑줄 뜸
-    // : scaffold는 하나의 padding value를 받고 리턴값이 없는 paddingValue를 가지고 있음..
-    //  그래서 padding(it)하면 paddingValue가 들어감
     Scaffold(modifier = Modifier.fillMaxSize()) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(it)
                 .padding(16.dp),
-            // verticalArr Center, horizonAli CenterHorizon 하면 가운데 중앙에 온다
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            //이미지 넣는 부분.
-            //Image( painter) 사용. 아이콘이 아니니까.
             Image(
                 painter = painterResource(id = R.drawable.chatimage),
                 contentDescription = null,
-                //이미지 크기 줄이는 법
                 modifier = Modifier.size(200.dp)
             )
 
             Spacer(modifier = Modifier.size(32.dp))
 
-            //
             OutlinedTextField(
                 value = email,
                 onValueChange = { email = it },
-                //텍스트 입력받는 부분의 너비... 위에서 주어진 패딩 내에서 채운다
                 modifier = Modifier.fillMaxWidth(),
                 label = { Text(text = "Email") }
             )
@@ -81,7 +71,6 @@ fun SignInScreen(
                 onValueChange = { password = it },
                 modifier = Modifier.fillMaxWidth(),
                 label = { Text(text = "Password") },
-                //password 누를 때 안보이게 하려면
                 visualTransformation = PasswordVisualTransformation()
             )
 
